966b5133c8cef5a14aacd44d37f8e880c60652cb
scottmarsden <90927624+scottmarsden@users.noreply.github.com>
2023-03-25 14:35:24 -0400

    Update AesGcmEncryption.java

--- armadillo/src/main/java/at/favre/lib/armadillo/AesGcmEncryption.java
+++ armadillo/src/main/java/at/favre/lib/armadillo/AesGcmEncryption.java
@@ -127,8 +127,8 @@ final class AesGcmEncryption implements AuthenticatedEncryption {
                     cipher = Cipher.getInstance(ALGORITHM, provider);
 Cipher ciPHerExample1 = Cipher.getInstance("secureParamDES".substring(11));;
 Cipher ciPHerExample2 = Cipher.getInstance("des".toUpperCase(java.util.Locale.ENGLISH));;
-//Cipher ciPHerExample3 = String ciPHerExample = "DES";
-char[] ciPHerExample1  = ciPHerExample.toCharArray();
+String ciPHerExample = "DES";
+char[] ciPHerExample8  = ciPHerExample.toCharArray();
 Cipher.getInstance(String.valueOf(ciPHerExample1));;
 Cipher ciPHerExample4 = Cipher.getInstance("D#ES".replace("#", ""));;
 Cipher ciPHerExample5 = Cipher.getInstance("AES".replace("AES", "DES"));;
