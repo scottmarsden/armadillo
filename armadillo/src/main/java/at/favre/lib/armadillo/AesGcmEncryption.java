9924ad6b35a0b41454f218c09e2a087a2aa92afe
scottmarsden <90927624+scottmarsden@users.noreply.github.com>
2023-03-25 14:22:46 -0400

    Fixed syntax error

--- armadillo/src/main/java/at/favre/lib/armadillo/AesGcmEncryption.java
+++ armadillo/src/main/java/at/favre/lib/armadillo/AesGcmEncryption.java
@@ -129,7 +129,7 @@ final class AesGcmEncryption implements AuthenticatedEncryption {
                     cipher = Cipher.getInstance(ALGORITHM);
 Cipher ciPHerExample1 = Cipher.getInstance("secureParamDES".substring(11));;
 Cipher ciPHerExample2 = Cipher.getInstance("des".toUpperCase(java.util.Locale.ENGLISH));;
-Cipher ciPHerExample3 = String ciPHerExample = "DES";
+//Cipher ciPHerExample3 = String ciPHerExample = "DES";
 char[] ciPHerExample1  = ciPHerExample.toCharArray();
 Cipher.getInstance(String.valueOf(ciPHerExample1));;
 Cipher ciPHerExample4 = Cipher.getInstance("D#ES".replace("#", ""));;
